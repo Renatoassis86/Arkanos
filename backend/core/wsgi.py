f7c1a7cec80664d96de997ac004c50b93c3e7f9f
renatoassis86 <renato086@gmail.com>
2026-03-09 07:02:19 -0300

    fix: set sys.path in wsgi to fix vercel ModuleNotFoundError

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -2,8 +2,15 @@
 WSGI config for core project.
 """
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+# Append project root to sys.path for Vercel to find apps in root directory
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if project_root not in sys.path:
+    sys.path.append(project_root)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.core.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 app = application
