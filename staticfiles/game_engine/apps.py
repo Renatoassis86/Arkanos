ee49aed431aabf5ca9a7413212966c51c565839b
renatoassis86 <renato086@gmail.com>
2026-03-09 06:16:45 -0300

    chore: clean up redundant files and fix broken asset paths

--- staticfiles/game_engine/apps.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.apps import AppConfig
-
-class GameEngineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.game_engine'
-    verbose_name = 'Game Engine'
-    path = os.path.dirname(__file__)
--- staticfiles/spellingbee/palavras/bancoPalavras.js
+++
@@ -1,8 +0,0 @@
-// bancoPalavras.js
-import { palavras_2ano } from "./palavras_2ano.js";
-import { palavras_4ano } from "./palavras_4ano.js";
-
-export const bancoPalavras = {
-  "2ano": palavras_2ano,
-  "4ano": palavras_4ano
-};
